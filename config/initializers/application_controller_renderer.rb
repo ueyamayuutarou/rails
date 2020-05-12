beac09e54fd728fa16e16326f1273b56c8839ee2
vagrant <vagrant@localhost.localdomain>
2020-05-12 07:26:16 +0000

    5/12

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '159195118bfde5b1afd8402d23e8a99f015eeef0122ba1d4374082203adfed3417c7dd144541541d2e42563f056bda3d8ae18b3c9d6b9210cc35cf4d0ca797c6'
--- db/migrate/20200510060730_create_lists.rb
+++ db/migrate/20200510060730_create_lists.rb
@@ -7,4 +7,3 @@ class CreateLists < ActiveRecord::Migration[5.2]
       t.timestamps
     end
   end
-end
